8512bcde23aa5d769acfb7371ce410197907fb00
supro1 <bikproml@rediffmail.com>
2021-10-14 17:53:11 +0530

    stage01 added

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -20,7 +20,7 @@ def get_data(config_path):
     local_data_dirs = config["local_data_dirs"]
 
     for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs,local_data_dirs),total=2,desc="list of folders",colour="red"):
-        create_directory(local_data_dir])
+        create_directory([local_data_dir])
         copy_file(source_download_dir,local_data_dir)
     
 
