cf4af3e23bebd7037b1f93f04339d9f6a9f957c4
supro1 <bikproml@rediffmail.com>
2021-10-23 10:19:05 +0530

    training complete

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import os
 import joblib
 import logging
-from src.utils.all_utils import 
+from src.utils.all_utils import get_timestamp
 
 
 def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
@@ -26,3 +26,18 @@ def create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
     ckpt_callback_filepath = os.path.join(callbacks_dir, "checkpoint_cb.cb")
     joblib.dump(checkpoint_callback, ckpt_callback_filepath)
     logging.info(f"tensorboard callback is being saved at {ckpt_callback_filepath}")
+
+
+def get_callbacks(callback_dir_path):
+
+    callback_path = [
+        os.path.join(callback_dir_path, bin_file) for bin_file in os.listdir(callback_dir_path) if bin_file.endswith(".cb")
+    ]
+
+    callbacks = [
+        joblib.load(path) for path in callback_path
+    ]
+
+    logging.info(f"saved callbacks are loaded from {callback_dir_path}")
+
+    return callbacks
