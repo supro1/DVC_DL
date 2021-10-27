1285b125430756085debe7ea0e6a2cbfc5d5a029
supro1 <bikproml@rediffmail.com>
2021-10-27 22:18:29 +0530

    stage 04 added

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -1,5 +1,5 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import load_full_model
+from src.utils.models import load_full_model, get_unique_path_to_save_model
 from src.utils.callbacks import get_callbacks
 from src.utils.data_management import train_valid_generator
 import argparse
@@ -37,7 +37,25 @@ def train_model(config_path, params_path):
         do_data_augmentation=params["AUGMENTATION"]
     )
 
+    steps_per_epoch = train_generator.samples // train_generator.batch_size
+    validation_steps = valid_generator.samples // valid_generator.batch_size
 
+    model.fit(
+        train_generator,
+        validation_data=valid_generator,
+        epochs=params["EPOCHS"], 
+        steps_per_epoch=steps_per_epoch, 
+        validation_steps=validation_steps,
+        callbacks=callbacks
+    )
+    logging.info(f"training completed")
+
+    trained_model_dir = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"])
+    create_directory([trained_model_dir])
+
+    model_file_path = get_unique_path_to_save_model(trained_model_dir)
+    model.save(model_file_path)
+    logging.info(f"trained model is saved at: {model_file_path}")
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
